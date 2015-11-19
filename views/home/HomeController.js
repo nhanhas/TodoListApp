447139549692e8157252e51b8e47363378ee6d66
MPereira <mpereira132@hotmail.com>
2015-11-19 22:07:41 +0000

    #S02

--- views/home/HomeController.js
+++ views/home/HomeController.js
@@ -7,6 +7,28 @@ app
 			$location.path(path);
 		};
 
+
+		//
+		$scope.parseXML = function (){
+				$scope.textFile = null;
+
+
+				var data = new Blob(['asdasda'], {type: 'text/plain'});
+
+				// If we are replacing a previously generated file we need to
+				// manually revoke the object URL to avoid memory leaks.
+				if ($scope.textFile !== null) {
+					window.URL.revokeObjectURL($scope.textFile);
+				}
+
+				$scope.textFile = window.URL.createObjectURL(data);
+
+				// returns a URL you can use as a href
+				return $scope.textFile;
+
+
+		};
+
 }]);
 
 
